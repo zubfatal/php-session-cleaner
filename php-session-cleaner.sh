c36f0b2314e36a5e28a765ae8a94dccc3021b948
zubfatal <zubfatal@users.noreply.github.com>
2015-11-05 16:56:41 +0100

    Create php-session-cleaner.sh

---
+++ php-session-cleaner.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# path(s) containing session data
+session_path="/var/www/*/tmp"
+
+# session lifetime in minutes
+session_lifetime=24
+
+# session filename mask
+session_mask="sess_*"
+
+# time prefix (see: man find)
+time_prefix="+"
+
+# delete command
+del_cmd="-exec rm {} \;"
+
+# enable debugging (changes $del_cmd to '-ls')
+debug=0
+
+### begin script ###
+script_dir=$( dirname ${0} )
+session_path_file=${1:-${script_dir}/php-session-cleaner.list}
+
+if [ ${debug:-0} -eq 1 ]; then
+        del_cmd="-ls"
+fi
+
+for _path in ${session_path} ; do
+        if [ -f "${session_path_file}" ]; then
+                _match=$( egrep "^${_path}\:" "${session_path_file}" )
+                _time=$( echo "${_match}" | awk -F":" '{ print $NF }' | sed -r 's/^[^0-9]+//g' )
+        fi
+
+        find ${_path} -ignore_readdir_race -type f -iname '${session_mask}' -mmin ${time_prefix}${_time:-${session_lifetime}} ${del_cmd}
+done

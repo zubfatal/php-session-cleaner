95aacf205c972532294aa89135726f5f0f8b2dcc
Tim Johannessen <tj@azero.dk>
2016-04-26 17:50:19 +0200

    Script no longer breaks if default  does not exist

--- README.md
+++ README.md
@@ -8,10 +8,15 @@ php-session-cleaner.sh [session-path-file]
 
 ## Custom session values
 Each path can have a custom session lifetime value - see [php-session-cleaner.list-example](./php-session-cleaner.list-example)
-The script will look for a file name **php-session-cleaner.list** in the same directory as the script itself, unless the file is passed as an argument.
+The script will look for a file name `php-session-cleaner.list` in the same directory as the script itself, unless the file is passed as an argument.
+
+### php-session-cleaner.list syntax
+\<path/to/session files\>:\<max session lifetime in minutes\>
+
+A value of `-1` will prevent cleanup. Can be defined or overridden per directory.
 
 ## Per directory override
-To better accommodate an environment where the user has access to the session data directory, but not the script or the php-session-cleaner.list file - a **.php-session-cleaner** file can be placed within the session data directory, containing the session lifetime value. This value will overrule the value of the **php-session-cleaner.list** file.
+To better accommodate an environment where the user has access to the session data directory, but not the script or the php-session-cleaner.list file, a `.php-session-cleaner` file can be placed within the session data directory, containing the session lifetime value. This value will overrule the value of the `php-session-cleaner.list` file.
 
 ## Crontab example
 ```
--- php-session-cleaner.sh
+++ php-session-cleaner.sh
@@ -16,25 +16,80 @@ time_prefix="+"
 del_cmd="-print0 | xargs -0 -r rm -f"
 
 # enable debugging (changes $del_cmd to '-ls')
-debug=0	
+debug=0
+
+# find files and list (debug) or delete them
+function process_files()
+{
+    local _path=$1
+    local _time=$2
+
+    eval "find ${_path} -ignore_readdir_race -type f -mmin ${time_prefix}${_time} -iname '${session_mask}' ${del_cmd}"
+}
+
+# get session lifetime value from file
+function get_session_lifetime_from_file()
+{
+    local _file=${1}
+    local _ret=""
+
+    if [ -f ${_file} ]; then
+        _ret=$( egrep "^[0-9]+$" ${_file} | head -n1 | sed -r 's/^[0-9]+//g' )
+    fi
+
+    echo ${_ret}
+}
+
 
 ### begin script ###
 script_dir=$( dirname ${0} )
 session_path_file=${1:-${script_dir}/php-session-cleaner.list}
 
 if [ ${debug:-0} -eq 1 ]; then
-        del_cmd="-ls"
+    del_cmd="-ls"
 fi
 
-for _path in ${session_path} ; do
+
+# $session_path_file exists
+if [ -f "${session_path_file}" ]; then
+
+    OIFS=$IFS
+    IFS=$'\n'
+
+    for _line in $( egrep "^/" ${session_path_file} ) ; do
+        _path=$( echo "${_line}" | awk -F":" '{ print $1 }' )
+        _file_time=$( echo "${_line}" | awk -F":" '{ print $NF }' | sed -r 's/^([0-9]+)(.*)$/\1/g' )
+        _path_time=$( get_session_lifetime_from_file ${_path}/.php-session-cleaner )
+
+        [[ ! -d "${_path}" ]] && continue
+
+        # no time provided in file
+        [[ "${_path}" == "${_file_time}" ]] && _file_time=${session_lifetime}
+
+        # no override found in .php-session-cleaner
+        [[ -z "${_path_time}" ]] && _time=${_file_time} || _time=${_path_time}
+
+        # process files if _time > 0 otherwise, don't clean
+        [[ ${_time} -gt 0 ]] && process_files "${_path}" ${_time}
+
+    done
+
+    IFS=$OIFS
+
+else
+    
+    # $session_path_file argument not given, use default $session_path
+
+    for _path in ${session_path} ; do
         [[ ! -d ${_path} ]] && continue
 
-        if [ -f "${_path}/.php-session-cleaner" ]; then
-                _time=$( egrep "^[0-9]+$" "${_path}/.php-session-cleaner" | tail -1 )
-        elif [ -f "${session_path_file}" ]; then
-                _match=$( egrep "^${_path}\:" "${session_path_file}" )
-                _time=$( echo "${_match}" | awk -F":" '{ print $NF }' | sed -r 's/^[^0-9]+//g' )
-        fi
+        # check override file
+        _path_time=$( get_session_lifetime_from_file ${_path}/.php-session-cleaner )
+        [[ -z "${_path_time}" ]] && _time=${session_lifetime} || _time=${_path_time}
 
-        eval "find ${_path} -ignore_readdir_race -type f -iname '${session_mask}' -mmin ${time_prefix}${_time:-${session_lifetime}} ${del_cmd}"
-done
+        # process files if _time > 0 otherwise, don't clean
+        [[ ${_time} -gt 0 ]] && process_files "${_path}" ${_time}
+    done
+
+fi
+
